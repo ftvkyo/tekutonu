2ba55444b3ddf7564be1c0570f65b46b42deb1bb
Andrei Stepanenko <ftvkyo@gmail.com>
2022-10-19 19:34:50 +0100

    fn create_swapchain_and_images

--- src/bin/game.rs
+++ src/bin/game.rs
@@ -124,6 +124,59 @@ fn choose_device_and_queue(
     (device, queues.next().unwrap())
 }
 
+fn create_swapchain_and_images(device: Arc<Device>, surface: Arc<Surface<Window>>) -> (Arc<Swapchain<Window>>, Vec<Arc<SwapchainImage<Window>>>) {
+    // We will only be allowed to request capabilities that are supported by the
+    // surface
+    let surface_capabilities = device
+        .physical_device()
+        .surface_capabilities(&surface, Default::default())
+        .unwrap();
+
+    // Internal format of the image buffers of the swapchain
+    let image_format = Some(
+        device
+            .physical_device()
+            .surface_formats(&surface, Default::default())
+            .unwrap()[0]
+            .0,
+    );
+
+    // Size of image buffers of the swapchain
+    let image_extent = match surface_capabilities.current_extent {
+        Some(extent) => extent,
+        None => surface.window().inner_size().into(),
+    };
+
+    Swapchain::new(
+        device.clone(),
+        surface.clone(),
+        SwapchainCreateInfo {
+            image_format,
+            image_extent,
+
+            // Can never create less than what surface allows.
+            min_image_count: surface_capabilities.min_image_count,
+
+            // We can only use images in the ways we have requested.
+            image_usage: ImageUsage {
+                color_attachment: true,
+                ..ImageUsage::empty()
+            },
+
+            // TODO: read more. maybe this means transparent window and compositing with other
+            // windows?
+            composite_alpha: surface_capabilities
+                .supported_composite_alpha
+                .iter()
+                .next()
+                .unwrap(),
+
+            ..Default::default()
+        },
+    )
+    .unwrap()
+}
+
 fn main() {
     let instance = make_instance();
 
@@ -135,58 +188,7 @@ fn main() {
     let (device, queue) = choose_device_and_queue(instance.clone(), surface.clone());
 
     // Allocating color (image) buffers through creating a swapchain.
-    let (mut swapchain, images) = {
-        // We will only be allowed to request capabilities that are supported by the
-        // surface
-        let surface_capabilities = device
-            .physical_device()
-            .surface_capabilities(&surface, Default::default())
-            .unwrap();
-
-        // Internal format of the image buffers of the swapchain
-        let image_format = Some(
-            device
-                .physical_device()
-                .surface_formats(&surface, Default::default())
-                .unwrap()[0]
-                .0,
-        );
-
-        // Size of image buffers of the swapchain
-        let image_extent = match surface_capabilities.current_extent {
-            Some(extent) => extent,
-            None => surface.window().inner_size().into(),
-        };
-
-        Swapchain::new(
-            device.clone(),
-            surface.clone(),
-            SwapchainCreateInfo {
-                image_format,
-                image_extent,
-
-                // Can never create less than what surface allows.
-                min_image_count: surface_capabilities.min_image_count,
-
-                // We can only use images in the ways we have requested.
-                image_usage: ImageUsage {
-                    color_attachment: true,
-                    ..ImageUsage::empty()
-                },
-
-                // TODO: read more. maybe this means transparent window and compositing with other
-                // windows?
-                composite_alpha: surface_capabilities
-                    .supported_composite_alpha
-                    .iter()
-                    .next()
-                    .unwrap(),
-
-                ..Default::default()
-            },
-        )
-        .unwrap()
-    };
+    let (mut swapchain, images) = create_swapchain_and_images(device.clone(), surface.clone());
 
     // How we are going to give data to the device
     #[repr(C)]
