bcd7ab46056ab43a8930b2baf869e3a23f6c9625
Andrei Stepanenko <ftvkyo@gmail.com>
2022-11-13 14:20:36 +0000

    Simplify Chunks with [][][] and Regions with `ndarray`

--- src/model/chunk.rs
+++ src/model/chunk.rs
@@ -4,57 +4,43 @@ use super::{
     types as t,
 };
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct Chunk {
-    blocks: [Block; c::CHUNK_TOTAL_BLOCKS],
+    blocks: [[[Block; c::CHUNK_Z_BLOCKS]; c::CHUNK_Y_BLOCKS]; c::CHUNK_X_BLOCKS],
 }
 
 impl Chunk {
-    pub fn new() -> Self {
-        Self {
-            blocks: [Block::air(); c::CHUNK_TOTAL_BLOCKS],
-        }
+    pub fn get_block<'s>(&'s self, loc: t::PointIntLocal) -> &'s Block {
+        &self.blocks[loc[0]][loc[1]][loc[2]]
     }
 
-    const fn to_index(location: t::PointIntLocal) -> usize {
-        let [x, y, z] = location;
-        z * c::CHUNK_ONE_SLICE_BLOCKS + y * c::CHUNK_ONE_COLUMN_BLOCKS + x
+    pub fn get_block_mut(&mut self, loc: t::PointIntLocal) -> &mut Block {
+        &mut self.blocks[loc[0]][loc[1]][loc[2]]
     }
 
-    const fn to_location(index: usize) -> t::PointIntLocal {
-        let x = index % c::CHUNK_ONE_COLUMN_BLOCKS;
-        let y = index % c::CHUNK_ONE_SLICE_BLOCKS / c::CHUNK_ONE_COLUMN_BLOCKS;
-        let z = index / c::CHUNK_ONE_SLICE_BLOCKS;
-        [x, y, z]
-    }
-
-    pub fn get_block<'s>(&'s self, location: t::PointIntLocal) -> &'s Block {
-        &self.blocks[Self::to_index(location)]
-    }
-
-    pub fn get_block_mut(&mut self, location: t::PointIntLocal) -> &mut Block {
-        &mut self.blocks[Self::to_index(location)]
-    }
-
-    pub fn set_block(&mut self, location: t::PointIntLocal, block: Block) {
-        self.blocks[Self::to_index(location)] = block;
+    pub fn set_block(&mut self, loc: t::PointIntLocal, block: Block) {
+        self.blocks[loc[0]][loc[1]][loc[2]] = block;
     }
 
     fn assemble_faces(&self) -> Vec<[t::PointIntGlobal; 4]> {
         let mut faces = Vec::<[t::PointIntGlobal; 4]>::new();
 
-        for (i, block) in self.blocks.iter().enumerate() {
-            if block.kind == BlockKind::Stone {
-                let block_offset = Self::to_location(i);
-                for face in c::BLOCK_FACES {
-                    let face = face.map(|v| {
-                        [
-                            v[0] + block_offset[0] as i64,
-                            v[1] + block_offset[1] as i64,
-                            v[2] + block_offset[2] as i64,
-                        ]
-                    });
-                    faces.push(face);
+        for x in 0..c::CHUNK_X_BLOCKS {
+            for y in 0..c::CHUNK_Y_BLOCKS {
+                for z in 0..c::CHUNK_Z_BLOCKS {
+                    let block = self.get_block([x, y, z]);
+                    if block.kind == BlockKind::Stone {
+                        for face in c::BLOCK_FACES {
+                            let face = face.map(|v| {
+                                [
+                                    v[0] + x as i64,
+                                    v[1] + y as i64,
+                                    v[2] + z as i64,
+                                ]
+                            });
+                            faces.push(face);
+                        }
+                    }
                 }
             }
         }
@@ -65,33 +51,35 @@ impl Chunk {
     pub fn get_render_data(&self, global_offset: [f32; 3]) -> (Vec<[f32; 3]>, Vec<usize>) {
         let faces = self.assemble_faces();
 
-        let to_f32 = |c| c as f32;
+        let to_f32 = |[x, y, z]: [i64; 3]| {
+            [
+                x as f32,
+                y as f32,
+                z as f32,
+            ]
+        };
+        let add_offset = |c: [f32; 3]| {
+            [
+                c[0] + global_offset[0],
+                c[1] + global_offset[1],
+                c[2] + global_offset[2],
+            ]
+        };
 
         let mut vertices = vec![];
         let mut indices = vec![];
 
         for face in faces {
             let i = vertices.len();
-            vertices.extend(face);
-
-            indices.push(i+0);
-            indices.push(i+1);
-            indices.push(i+2);
-            indices.push(i+0);
-            indices.push(i+2);
-            indices.push(i+3);
+            // Four faces
+            vertices.extend(
+                face.into_iter()
+                    .map(to_f32)
+                    .map(add_offset)
+            );
+            // Two triangles
+            indices.extend([i, i+1, i+2, i, i+2, i+3]);
         }
-
-        let vertices = vertices.into_iter()
-            .map(|v| v.map(to_f32))
-            .map(|v| {
-                [
-                    v[0] + global_offset[0],
-                    v[1] + global_offset[1],
-                    v[2] + global_offset[2],
-                ]
-            })
-            .collect();
 
         (vertices, indices)
     }
@@ -101,112 +89,12 @@ impl Chunk {
 mod tests {
     use super::*;
 
-    const X: usize = c::CHUNK_X_BLOCKS;
-    const Y: usize = c::CHUNK_Y_BLOCKS;
-    const Z: usize = c::CHUNK_Z_BLOCKS;
-    const T: usize = c::CHUNK_TOTAL_BLOCKS;
-
-    mod to_index {
-        use super::*;
-
-        #[test]
-        fn origin() {
-            assert_eq!(
-                Chunk::to_index([0, 0, 0]),
-                0,
-                "indexing should start at 0 for [0, 0, 0]"
-            );
-        }
-
-        #[test]
-        fn last() {
-            assert_eq!(
-                Chunk::to_index([X - 1, Y - 1, Z - 1]),
-                T - 1,
-                "max XYZ should yield max index"
-            );
-        }
-
-        #[test]
-        fn growth() {
-            assert_eq!(
-                Chunk::to_index([1, 0, 0]),
-                1,
-                "X should be least significant"
-            );
-            assert_eq!(
-                Chunk::to_index([0, 1, 0]),
-                X,
-                "Y should be the middle significant"
-            );
-            assert_eq!(
-                Chunk::to_index([0, 0, 1]),
-                X * Y,
-                "Z should be the most significant"
-            );
-        }
-
-        #[test]
-        fn arbitrary_333() {
-            assert_eq!(
-                Chunk::to_index([3, 3, 3]),
-                X * 3 + X * Y * 3 + 3,
-                "[3, 3, 3] converts correctly"
-            );
-        }
-    }
-
-    mod to_location {
-        use super::*;
-
-        #[test]
-        fn origin() {
-            assert_eq!(Chunk::to_location(0), [0, 0, 0], "0 should be [0, 0, 0]");
-        }
-
-        #[test]
-        fn last() {
-            assert_eq!(
-                Chunk::to_location(T - 1),
-                [X - 1, Y - 1, Z - 1],
-                "max index should yield max XYZ"
-            );
-        }
-
-        #[test]
-        fn growth() {
-            //
-            assert_eq!(
-                Chunk::to_location(1),
-                [1, 0, 0],
-                "X should be the least significant"
-            );
-            assert_eq!(
-                Chunk::to_location(X),
-                [0, 1, 0],
-                "Y should be the middle significant"
-            );
-            assert_eq!(
-                Chunk::to_location(X * Y),
-                [0, 0, 1],
-                "Z should be the most significant"
-            );
-        }
-
-        #[test]
-        fn parity_with_to_index() {
-            for index in 0..T {
-                assert_eq!(Chunk::to_index(Chunk::to_location(index)), index);
-            }
-        }
-    }
-
     mod assemble_faces {
         use super::*;
 
         #[test]
         fn empty() {
-            let chunk = Chunk::new();
+            let chunk = Chunk::default();
 
             let ts = chunk.assemble_faces();
 
@@ -215,7 +103,7 @@ mod tests {
 
         #[test]
         fn one() {
-            let mut chunk = Chunk::new();
+            let mut chunk = Chunk::default();
             chunk.get_block_mut([0, 0, 0]).kind = BlockKind::Stone;
 
             let ts = chunk.assemble_faces();
@@ -225,7 +113,7 @@ mod tests {
 
         #[test]
         fn two() {
-            let mut chunk = Chunk::new();
+            let mut chunk = Chunk::default();
             chunk.get_block_mut([0, 0, 0]).kind = BlockKind::Stone;
             chunk.get_block_mut([0, 0, 1]).kind = BlockKind::Stone;
 
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -6,18 +6,3 @@ mod game_model;
 pub mod region;
 pub mod types;
 pub use game_model::*;
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn accessing_block_in_region() {
-        let reg = region::Region::new();
-        let chunk = reg.get_chunk([1, 2, 3]);
-        let block = chunk.get_block([1, 2, 3]);
-
-        assert_eq!(block.kind, block::BlockKind::Air);
-    }
-}
